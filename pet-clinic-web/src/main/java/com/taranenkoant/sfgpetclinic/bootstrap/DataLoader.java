1c8ef0dd7b8d5056a8183526598d2b9d459b97c4
Anton <taranenkoant@gmail.com>
2019-11-02 17:04:45 +0300

    Update Map impl closes #18

--- pet-clinic-web/src/main/java/com/taranenkoant/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/taranenkoant/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mickhael");
         owner1.setLastName("Wetson");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fioona");
         owner2.setLastName("Glinvane");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Axewwd");
         vet2.setLastName("Sassa");
 
