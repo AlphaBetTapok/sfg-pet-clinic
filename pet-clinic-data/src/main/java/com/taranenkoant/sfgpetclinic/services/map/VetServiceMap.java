548fc658178e11b1fca5a3018d669c40055ca09e
Anton <taranenkoant@gmail.com>
2019-11-02 16:04:20 +0300

    adding spring config closes #17

--- pet-clinic-data/src/main/java/com/taranenkoant/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/taranenkoant/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.taranenkoant.sfgpetclinic.services.map;
 
 import com.taranenkoant.sfgpetclinic.model.Vet;
 import com.taranenkoant.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
--- pet-clinic-web/src/main/java/com/taranenkoant/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/taranenkoant/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.taranenkoant.sfgpetclinic.model.Owner;
 import com.taranenkoant.sfgpetclinic.model.Vet;
 import com.taranenkoant.sfgpetclinic.services.OwnerService;
 import com.taranenkoant.sfgpetclinic.services.VetService;
-import com.taranenkoant.sfgpetclinic.services.map.OwnerServiceMap;
-import com.taranenkoant.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
