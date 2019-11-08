1162a256e54e07be5d39df7f256e88421297774f
Антон Тараненко <taranenkoant@gmail.com>
2019-11-08 17:25:21 +0300

    added specialities map impl. closes #26

---
+++ pet-clinic-data/src/main/java/com/taranenkoant/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.taranenkoant.sfgpetclinic.services;
+
+import com.taranenkoant.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
