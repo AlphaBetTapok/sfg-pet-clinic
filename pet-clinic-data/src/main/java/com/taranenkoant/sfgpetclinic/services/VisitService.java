d5db77dc254931e6036aacc4814c5bafa5dde439
Антон Тараненко <taranenkoant@gmail.com>
2019-11-25 19:05:12 +0300

    Impl map visit service. closes #41

---
+++ pet-clinic-data/src/main/java/com/taranenkoant/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.taranenkoant.sfgpetclinic.services;
+
+import com.taranenkoant.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/taranenkoant/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.taranenkoant.sfgpetclinic.services.map;
+
+import com.taranenkoant.sfgpetclinic.model.Visit;
+import com.taranenkoant.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null)
+            throw new RuntimeException(("invalid visit"));
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
